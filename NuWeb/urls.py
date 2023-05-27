15e2705181b10f7b053d383de1d5688792a58cc0
JEFRY200307 <jefferson.flores.m@uni.pe>
2023-05-27 01:16:22 -0500

    my first commit-index and styles

--- NuWeb/urls.py
+++ NuWeb/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from contactanos.views import contactos
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", contactos)
 ]
